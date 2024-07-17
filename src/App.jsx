17a4aa48ded4383f1b4d2dab5bb51485f6e32e25
Samuel Rebuelta-Sanchez <srebueltasanchez@srebuel-ltmbymq.internal.salesforce.com>
2024-07-17 10:18:27 -0700

    complete modal slide show functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css'
+import "./App.css";
+import ElectionEducation from "./Components/HomePage/ElectionEducation/ElectionEducation";
 
 function App() {
-
   return (
     <>
       <h1>Hi</h1>
+      <ElectionEducation />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
