084ba03ad23fa22ad9a3bd87cc8bb8a5c7dfddec
Samuel Rebuelta-Sanchez <srebueltasanchez@srebuel-ltmbymq.internal.salesforce.com>
2024-07-18 19:57:32 -0700

    embed google maps in election modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,41 @@
-
 import ElectionEducation from "./Components/HomePage/ElectionEducation/ElectionEducation";
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import AboutSection from "./Components/HomePage/AboutSection/AboutSection.jsx"
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AboutSection from "./Components/HomePage/AboutSection/AboutSection.jsx";
 import Header from "./Components/Header/Header";
-import Post from './Components/Forum/Post/Post.jsx'
-import Forum from './Components/Forum/Forum';
-import HomePage from './Components/HomePage/HomePage';
-import ElectionResults from './Components/ElectionResults/ElectionResults';
-import SearchLocation from './Components/SearchLocation/SearchLocation';
-import Footer from './Components/Footer/Footer';
-import InteractiveMap from './Components/InteractiveMap/InteractiveMap';
+import Post from "./Components/Forum/Post/Post.jsx";
+import Forum from "./Components/Forum/Forum";
+import HomePage from "./Components/HomePage/HomePage";
+import ElectionResults from "./Components/ElectionResults/ElectionResults";
+import SearchLocation from "./Components/SearchLocation/SearchLocation";
+import Footer from "./Components/Footer/Footer";
+import InteractiveMap from "./Components/InteractiveMap/InteractiveMap";
+import ActualElection from "./Components/ActualElection/ActualElection.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <div id="root">
-          <Header/>
-          <main className="main-content">
-              <Routes>
-                  <Route path="/" element={<HomePage />} />
+        <Header />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
 
-                   {/* <ElectionEducation /> */}
-                   {/* <InteractiveMap/> */}
-                   {/* <AboutSection /> */}
+            {/* <ElectionEducation /> */}
+            {/* <InteractiveMap/> */}
+            {/* <AboutSection /> */}
 
-                  <Route path="/results" element={<ElectionResults />} />
-                  <Route path="/search" element={<SearchLocation />} />
-                  <Route path="/forum" element={<Forum />} />
-                  {/* Change to map */}
-                  <Route path="/map" element={<InteractiveMap/>} />
-              </Routes>
-              </main>
-              <Footer />
-          </div>
-        </BrowserRouter>
+            <Route path="/results" element={<ElectionResults />} />
+            <Route path="/search" element={<SearchLocation />} />
+            <Route path="/forum" element={<Forum />} />
+            {/* Change to map */}
+            <Route path="/map" element={<InteractiveMap />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
-
 }
 
 export default App;
--- src/Components/ElectionListing/ElectionListing.jsx
+++ src/Components/ElectionListing/ElectionListing.jsx
@@ -1,33 +1,50 @@
-import React, {useState, useEffect} from "react";
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import "./ElectionListing.css";
+import ElectionModal from "./ElectionModal/ElectionModal";
 
-function ElectionListing({electionData}) {
-    if (!electionData || electionData.elections === 0) {
-        return <p>No upcoming elections in your area</p>; 
-    }
+function ElectionListing({ electionData, uriAddress }) {
+  const [showModal, setShowModal] = useState(false);
 
-    const formatDate = (dateString) => {
-        const options = { year: 'numeric', month: 'long', day: 'numeric' };
-        return new Date(dateString).toLocaleDateString(undefined, options);
-    };
+  if (!electionData || electionData.elections === 0) {
+    return <p>No upcoming elections in your area</p>;
+  }
 
-    
-    return(
+  const formatDate = (dateString) => {
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
+
+  return (
+    <div className="election-listing">
+      {electionData.map((election, index) => (
         <>
-            <div className="election-listing">
-                {electionData.map((election, index) => (
-                    <div className="each-election" key={index}>
-                        <h3 className="election-name">
-                            <Link to={`/election/${index}`} className="election-name-link">
-                                {election.name}
-                            </Link>
-                        </h3>
-                        <p className="election-day">Election Day: {formatDate(election.electionDay)}</p>
-                    </div>
-                ))}
-            </div>
+          <div className="each-election" key={index}>
+            <h3
+              className="election-name election-name-link"
+              onClick={() => {
+                setShowModal(true);
+              }}
+            >
+              {election.name}
+            </h3>
+            <p className="election-day">
+              Election Day: {formatDate(election.electionDay)}
+            </p>
+          </div>
+          {showModal && (
+            <ElectionModal
+              onClose={() => {
+                setShowModal(false);
+              }}
+              electionId={election.id}
+              electionName={election.name}
+              address={uriAddress}
+            />
+          )}
         </>
-    )
+      ))}
+    </div>
+  );
 }
 export default ElectionListing;
