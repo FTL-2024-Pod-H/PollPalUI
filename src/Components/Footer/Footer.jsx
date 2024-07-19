233373422c8ed96cf22175c93df482998a095d4b
Lucky Beulla Muhoza <muhozaluckyb03@gmail.com>
2024-07-19 11:28:59 -0700

    in-progress css

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Footer.css';
-import linkedinIcon from "/assets/icons8-linkedin-50.png";
-import gmailIcon from "/assets/icons8-gmail-50.png";
+import linkedinIcon from "/assets/icons8-linkedin-48.png";
+import githubIcon from "/assets/github-logo-git-hub-icon-on-white-background-free-vector.jpg";
 
 const Footer = () => {
     return (
@@ -9,40 +9,51 @@ const Footer = () => {
             <div className="footer-left">
                 <div className="branding">
                     <img src="/assets/poll-pal-icon.png" alt="Poll Pal Icon" className="poll-pal-icon" />
-                    <p className="description">PollPal</p>
+                    <p className="description">Poll<span className="poll-white">Pal</span></p>
                 </div>
-                <p className="copyright">© 2024 Future Force Tech Launchpad</p>
             </div>
-            <div className="footer-right">
+            <div className="footer-center">
                 <div className="students">
-                    <div className="student">
-                        <p className="student-name">Lucky Beulla Muhoza</p>
-                        <a href="https://www.linkedin.com/in/luckymuhoza/" target="_blank" rel="noopener noreferrer" className="linkedin-link">
-                            <img src={linkedinIcon} alt="LinkedIn" className="linkedin-icon" />
-                        </a>
-                        <a href="mailto:muhozaluckyb03@gmail.com" target="_blank" rel="noopener noreferrer" className="linkedin-link">
-                            <img src={gmailIcon} alt="Gmail" className="linkedin-icon" />
-                        </a>
-                    </div>
-                    <div className="student">
-                        <p className="student-name">Samuel Rubuelta-Sanchez</p>
-                        <a href="https://www.linkedin.com/in/luckymuhoza/" target="_blank" rel="noopener noreferrer" className="linkedin-link">
-                            <img src={linkedinIcon} alt="LinkedIn" className="linkedin-icon" />
-                        </a>
-                    </div>
-                    <div className="student">
-                        <p className="student-name">Hanna Abrahem</p>
-                        <a href="https://www.linkedin.com/in/luckymuhoza/" target="_blank" rel="noopener noreferrer" className="linkedin-link">
-                            <img src={linkedinIcon} alt="LinkedIn" className="linkedin-icon" />
-                        </a>
-                    </div>
                     <div className="student">
                         <p className="student-name">Kiahna Isadore</p>
                         <a href="https://www.linkedin.com/in/luckymuhoza/" target="_blank" rel="noopener noreferrer" className="linkedin-link">
                             <img src={linkedinIcon} alt="LinkedIn" className="linkedin-icon" />
                         </a>
+                        <a href="" target="_blank" rel="noopener noreferrer" className="gmail-link">
+                            <img src={githubIcon} alt="GitHub" className="github-icon" />
+                        </a>
+                    </div>
+                    <div className="student">
+                        <p className="student-name">Hanna Abrahem </p>
+                        <a href="https://www.linkedin.com/in/samuelrubuelta/" target="_blank" rel="noopener noreferrer" className="linkedin-link">
+                            <img src={linkedinIcon} alt="LinkedIn" className="linkedin-icon" />
+                        </a>
+                        <a href="" target="_blank" rel="noopener noreferrer" className="gmail-link">
+                            <img src={githubIcon} alt="GitHub" className="github-icon" />
+                        </a>
+                    </div>
+                    <div className="student">
+                        <p className="student-name">Lucky Beulla Muhoza</p>
+                        <a href="https://www.linkedin.com/in/hannaabrahem/" target="_blank" rel="noopener noreferrer" className="linkedin-link">
+                            <img src={linkedinIcon} alt="LinkedIn" className="linkedin-icon" />
+                        </a>
+                        <a href="https://github.com/luckybeulla" target="_blank" rel="noopener noreferrer" className="gmail-link">
+                            <img src={githubIcon} alt="GitHub" className="github-icon" />
+                        </a>
+                    </div>
+                    <div className="student">
+                        <p className="student-name">Samuel Rubuelta-Sanchez</p>
+                        <a href="https://www.linkedin.com/in/kiahnaisadore/" target="_blank" rel="noopener noreferrer" className="linkedin-link">
+                            <img src={linkedinIcon} alt="LinkedIn" className="linkedin-icon" />
+                        </a>
+                        <a href="" target="_blank" rel="noopener noreferrer" className="gmail-link">
+                            <img src={githubIcon} alt="GitHub" className="github-icon" />
+                        </a>
                     </div>
                 </div>
+            </div>
+            <div className="footer-right">
+                <p className="copyright">© 2024 Future Force Tech Launchpad</p>
             </div>
         </footer>
     );
