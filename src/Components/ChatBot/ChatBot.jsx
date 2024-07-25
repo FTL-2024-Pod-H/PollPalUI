35f54c5fde60ed3957a201ab6140a3f38fcced2f
Lucky Beulla Muhoza <muhozaluckyb03@gmail.com>
2024-07-25 16:12:02 -0700

    css-responsive-design

--- src/Components/ChatBot/ChatBot.jsx
+++ src/Components/ChatBot/ChatBot.jsx
@@ -92,7 +92,7 @@ const ChatBot = () => {
     };
 
     const getCollapseTooltip = () => {
-        return isCollapsed ? 'Expand Chatbot' : 'Minimize Chatbot';
+        return isCollapsed ? 'Expand' : 'Minimize';
     };
 
     const handleEmojiClick = (emoji) => {
@@ -120,7 +120,7 @@ const ChatBot = () => {
                             <button 
                                 onClick={handleCollapse} 
                                 className="button collapse-button"
-                                title={isCollapsed ? "Expand Chatbot" : "Collapse Chatbot"}
+                                // title={isCollapsed ? "Expand Chatbot" : "Collapse Chatbot"}
                             >
                                 <div className="icon">
                                     {isCollapsed ? (
@@ -140,12 +140,12 @@ const ChatBot = () => {
                             <button 
                                 onClick={handleClose} 
                                 className="button chat-close-button"
-                                title="Close Chatbot"
+                                // title="Close Chatbot"
                             >
                                 <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
                                     <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                                 </svg>
-                                <span className="tooltip-text">Close Chatbot</span>
+                                <span className="tooltip-text">Close</span>
                             </button>
                         </div>
                     </div>
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
                     <img src="/assets/poll-pal-icon.png" alt="Poll Pal Icon" className="poll-pal-icon" />
                     <p className="description">Poll<span className="poll-white">Pal</span></p>
                 </div>
-                <p className="copyright">© 2024 Future Force Tech Launchpad</p>
+                <p className="copyright">© 2024 Futureforce Tech Launchpad</p>
             </div>
             {/* <div className="footer-center">
                 <p className="copyright">© 2024 Future Force Tech Launchpad</p>
@@ -21,10 +21,10 @@ const Footer = () => {
                     <div className="student">
                         <p className="student-name">Kiahna Isadore</p>
                         <div className="student-icons">
-                            <a href="https://www.linkedin.com/in/luckymuhoza/" target="_blank" rel="noopener noreferrer" className="linkedin-link">
+                            <a href="https://www.linkedin.com/in/kiahna-isadore/" target="_blank" rel="noopener noreferrer" className="linkedin-link">
                                 <img src={linkedinIcon} alt="LinkedIn" className="linkedin-icon" />
                             </a>
-                            <a href="" target="_blank" rel="noopener noreferrer" className="github-link">
+                            <a href="https://github.com/Kisadore" target="_blank" rel="noopener noreferrer" className="github-link">
                                 <img src={githubIcon} alt="GitHub" className="github-icon" />
                             </a>
                         </div>
@@ -32,10 +32,10 @@ const Footer = () => {
                     <div className="student">
                         <p className="student-name">Hanna Abrahem</p>
                         <div className="student-icons">
-                            <a href="https://www.linkedin.com/in/samuelrubuelta/" target="_blank" rel="noopener noreferrer" className="linkedin-link">
+                            <a href="https://www.linkedin.com/in/hannaabra/" target="_blank" rel="noopener noreferrer" className="linkedin-link">
                                 <img src={linkedinIcon} alt="LinkedIn" className="linkedin-icon" />
                             </a>
-                            <a href="" target="_blank" rel="noopener noreferrer" className="github-link">
+                            <a href="https://github.com/hanna00112" target="_blank" rel="noopener noreferrer" className="github-link">
                                 <img src={githubIcon} alt="GitHub" className="github-icon" />
                             </a>
                         </div>
@@ -43,7 +43,7 @@ const Footer = () => {
                     <div className="student">
                         <p className="student-name">Lucky Beulla Muhoza</p>
                         <div className="student-icons">
-                            <a href="https://www.linkedin.com/in/hannaabrahem/" target="_blank" rel="noopener noreferrer" className="linkedin-link">
+                            <a href="https://www.linkedin.com/in/luckymuhoza/" target="_blank" rel="noopener noreferrer" className="linkedin-link">
                                 <img src={linkedinIcon} alt="LinkedIn" className="linkedin-icon" />
                             </a>
                             <a href="https://github.com/luckybeulla" target="_blank" rel="noopener noreferrer" className="github-link">
@@ -52,12 +52,12 @@ const Footer = () => {
                         </div>
                     </div>
                     <div className="student">
-                        <p className="student-name">Samuel Rubuelta-Sanchez</p>
+                        <p className="student-name">Samuel Rebuelta-Sanchez</p>
                         <div className="student-icons">
-                            <a href="https://www.linkedin.com/in/kiahnaisadore/" target="_blank" rel="noopener noreferrer" className="linkedin-link">
+                            <a href="https://www.linkedin.com/in/samuel-rebuelta/" target="_blank" rel="noopener noreferrer" className="linkedin-link">
                                 <img src={linkedinIcon} alt="LinkedIn" className="linkedin-icon" />
                             </a>
-                            <a href="" target="_blank" rel="noopener noreferrer" className="github-link">
+                            <a href="https://github.com/sam-reb" target="_blank" rel="noopener noreferrer" className="github-link">
                                 <img src={githubIcon} alt="GitHub" className="github-icon" />
                             </a>
                         </div>
