8d2ae22ae1066ddcb2213acbd81ee13fd80e318d
Samuel Rebuelta-Sanchez <srebueltasanchez@srebuel-ltmbymq.internal.salesforce.com>
2024-08-05 13:25:23 -0700

    show username on profile modal

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,9 +7,10 @@ const Header = () => {
   const [scrolled, setScrolled] = useState(false);
   const navigate = useNavigate();
   const [menuOpen, setMenuOpen] = useState(false);
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false); 
-  const [userAvatar, setUserAvatar] = useState(""); 
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [userAvatar, setUserAvatar] = useState("");
   const [id, setId] = useState(0);
+  const [username, setUsername] = useState("");
 
   const dropdownRef = useRef(null);
   const menuRef = useRef(null);
@@ -22,6 +23,8 @@ const Header = () => {
       if (payload) {
         setId(payload.userId);
         setUserAvatar(getUserAvatar(payload.userName));
+        setUsername(`@${payload.userName}`);
+        console.log(username);
       }
     } else {
       setId(null);
@@ -40,7 +43,6 @@ const Header = () => {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, [localStorage.getItem("token")]);
-
 
   useEffect(() => {
     const handleSignOut = (event) => {
@@ -109,7 +111,6 @@ const Header = () => {
     return `https://ui-avatars.com/api/?name=${username}&background=random`;
   }
 
-  
   return (
     <header className={`header ${scrolled ? "header-scrolled" : ""}`}>
       <div className="header-content">
@@ -126,14 +127,14 @@ const Header = () => {
             <span className="pal">Pal</span>
           </h1>
         </div>
-        <button 
-          className="menu-toggle" 
+        <button
+          className="menu-toggle"
           onClick={toggleMenu}
           // onClick={() => {{toggleMenu} {onclose}}}
         >
           &#9776;
         </button>
-        <div 
+        <div
           ref={menuRef}
           className={`nav-auth-container ${menuOpen ? "open" : ""}`}
         >
@@ -156,9 +157,10 @@ const Header = () => {
               </li>
             </ul>
           </nav>
-          <div className="auth-buttons">            
+          <div className="auth-buttons">
             {localStorage.getItem("token") ? (
               <>
+                <div className="phone-nav">
                   <img
                     src={userAvatar}
                     alt="User Avatar"
@@ -167,18 +169,23 @@ const Header = () => {
                     // onClick={() => { console.log("clicked");
                     // setIsDropdownOpen(!isDropdownOpen);}}
                   />
-                  {isDropdownOpen && (
-                    <div className="dropdown-menu" ref={dropdownRef}>
-                      <button onClick={handleLogout} className="animated-button">
-                        Sign out
-                      </button>
-                    </div>
-                  )}
-                  <div className="menu-signout open">
+                  <p className="username open" id="phone-username">
+                    {username}
+                  </p>
+                </div>
+                {isDropdownOpen && (
+                  <div className="dropdown-menu" ref={dropdownRef}>
+                    <p className="username open">{username}</p>
                     <button onClick={handleLogout} className="animated-button">
                       Sign out
                     </button>
                   </div>
+                )}
+                <div className="menu-signout open">
+                  <button onClick={handleLogout} className="animated-button">
+                    Sign out
+                  </button>
+                </div>
               </>
             ) : (
               <>
